5c86e574e15a5fc3e613dc4d598d69dabf754f7d
Brandon Apol <brandonapol@cedarville.edu>
2023-02-03 15:36:47 -0800

    initial arch commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <h1 className='text-4xl'>Welcome to the Blog</h1>
+  )
+}
+
+export default HomePage
---
+++ sonnyblog/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'ptjvgy2a',
+    dataset: 'production'
+  }
+})
