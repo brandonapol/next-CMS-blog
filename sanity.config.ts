6faf69ae27b29323c26ac88a3ee8c2494d65f7f0
Brandon Apol <brandonapol@cedarville.edu>
2023-02-06 13:06:35 -0900

    CMS works

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,12 +3,15 @@ import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
-export default defineConfig({
-  name: 'default',
-  title: 'sonnyblog',
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 
-  projectId: 'ptjvgy2a',
-  dataset: 'production',
+export default defineConfig({
+  basePath: "/studio",
+  name: 'boredalaskan_apps',
+  title: 'boredalaskan_apps',
+  projectId,
+  dataset,
 
   plugins: [deskTool(), visionTool()],
 
